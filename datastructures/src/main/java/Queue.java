e21128ca3ebda50e0ecbbfcd4351486b93a775ad
Martin Ponce <doc.brown.83@gmail.com>
2018-02-18 00:22:11 +0800

    Implemented Queue, bdd tests now green.

--- datastructures/src/main/java/Queue.java
+++ datastructures/src/main/java/Queue.java
@@ -1,22 +1,85 @@
 public class Queue<T> {
 
-    public void add(T data) {
-        throw new UnsupportedOperationException();
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public Queue() {
+        head = null;
+        tail = null;
     }
 
+    /**
+     * Add stuff to end of queue.
+     * If empty, add as first element.
+     * If no tail, add as tail.
+     * If is tail, add after tail.
+     *
+     * @param data
+     */
+    public void add(T data) {
+
+        Node node = new Node(data);
+
+        if (isEmpty())
+            head = node;
+
+        if (tail != null)
+            tail.next = node;
+
+        tail = node;
+        count++;
+    }
+
+    /**
+     * Remove stuff from start of queue.
+     * If empty, there's nothing to remove.
+     * Otherwise, save the data being removed so we can return it at the end.
+     * Remove/dereference the first element by pushing forward the second element.
+     * If first element is now empty, clear out the last element too, we've deleted everything.
+     * Return the thing being removed.
+     *
+     * @return
+     */
     public T remove() {
-        throw new UnsupportedOperationException();
+
+        if (isEmpty())
+            return null;
+
+        T toRemove = peek();
+        head = head.next;
+
+        if (head == null)
+            tail = null;
+
+        count--;
+        return toRemove;
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return count;
     }
 
     private boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return head == null;
     }
 
     private T peek() {
-        throw new UnsupportedOperationException();
+
+        if (isEmpty())
+            return null;
+
+        return head.data;
+    }
+
+    private class Node {
+
+        private T data;
+        private Node next;
+
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
     }
 }
