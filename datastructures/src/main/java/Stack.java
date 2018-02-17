d62d303ce4fa9f76ca9fb4cf8f557bc993f0ac69
Martin Ponce <doc.brown.83@gmail.com>
2018-02-17 22:55:13 +0800

    Implemented Stack, bdd tests now green.

--- datastructures/src/main/java/Stack.java
+++ datastructures/src/main/java/Stack.java
@@ -1,14 +1,69 @@
+/**
+ * Implementation of stack.
+ *
+ * @param <T> The type to store in the stack
+ */
 public class Stack<T> {
 
-    public void push(T data) {
-        throw new UnsupportedOperationException();
+    private Node head;
+    private int count;
+
+    public Stack() {
+        head = null;
     }
 
+    /**
+     * Add stuff to the stack. Pushes current element down and places new element at top of stack.
+     *
+     * @param data
+     */
+    public void push(T data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+        count++;
+    }
+
+    /**
+     * Delete stuff from the stack and return the thing being deleted. Raises second element to top of stack,
+     * deleting the top element, then return the data about to be deleted.
+     *
+     * @return the top element.
+     */
     public T pop() {
-        throw new UnsupportedOperationException();
+
+        if (isEmpty())
+            return null;
+
+        T toDelete = peek();
+        head = head.next;
+        count--;
+        return toDelete;
+    }
+
+    private boolean isEmpty() {
+        return head == null;
+    }
+
+    private T peek() {
+        if (isEmpty())
+            return null;
+
+        return head.data;
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return count;
+    }
+
+    private class Node {
+
+        private T data;
+        private Node next;
+
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
     }
 }
--- datastructures/src/test/resources/stack_reverse_word_test.story
+++ datastructures/src/test/resources/stack_reverse_word_test.story
@@ -11,23 +11,23 @@ When I pop the stack
 Then the order of the word is reversed to <reverse>
 
 Examples:
-|word|reverse|
-|desserts|stressed|
-|god|dog|
-|live|evil|
-|lived|devil|
-|knits|stink|
-|regal|lager|
-|pupils|slipup|
-|raw|war|
-|smart|trams|
-|pals|slap|
-|snug|guns|
-|straw|warts|
-|time|emit|
-|deliver|reviled|
-|star|rats|
-|spoons|snoops|
-|wow|wow|
-|mum|mum|
-|01234|43210|
+| word     | reverse  |
+| desserts | stressed |
+| god      | dog      |
+| live     | evil     |
+| lived    | devil    |
+| knits    | stink    |
+| regal    | lager    |
+| pupils   | slipup   |
+| raw      | war      |
+| smart    | trams    |
+| pals     | slap     |
+| snug     | guns     |
+| straw    | warts    |
+| time     | emit     |
+| deliver  | reviled  |
+| star     | rats     |
+| spoons   | snoops   |
+| wow      | wow      |
+| mum      | mum      |
+| 01234    | 43210    |
