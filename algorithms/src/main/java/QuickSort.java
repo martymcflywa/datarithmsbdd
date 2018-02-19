fd0c24e7071a7d71559fee124a07e38d019b4e1c
Martin Ponce <doc.brown.83@gmail.com>
2018-02-19 18:20:51 +0800

    Implemented quick sort, test green.

--- algorithms/src/main/java/QuickSort.java
+++ algorithms/src/main/java/QuickSort.java
@@ -1,6 +1,39 @@
 public class QuickSort {
 
     public void sort(String[] data) {
-        throw new UnsupportedOperationException();
+        sort(data, 0, data.length - 1);
+    }
+
+    private void sort(String[] data, int left, int right) {
+        if (left >= right)
+            return;
+
+        String pivot = data[(left + right) / 2];
+        int index = partition(data, left, right, pivot);
+        sort(data, left, index - 1);
+        sort(data, index, right);
+    }
+
+    private int partition(String[] data, int left, int right, String pivot) {
+
+        while (left <= right) {
+            while (data[left].compareTo(pivot) < 0)
+                left++;
+            while (data[right].compareTo(pivot) > 0)
+                right--;
+
+            if (left <= right) {
+                swap(data, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    private void swap(String[] data, int left, int right) {
+        String temp = data[left];
+        data[left] = data[right];
+        data[right] = temp;
     }
 }
